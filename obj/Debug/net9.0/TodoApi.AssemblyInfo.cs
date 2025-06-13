6286f8b0308ec93066c7b9b5df2bf4c409847573
Matthew Waldeck <112938730+MattWaldeck@users.noreply.github.com>
2025-06-13 10:51:32 +0200

    update

--- obj/Debug/net9.0/TodoApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e97ea05983cf8d68869491d35d957af30968284d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+724cde512bfbdd15b8a685630e3deb65293e0547")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
