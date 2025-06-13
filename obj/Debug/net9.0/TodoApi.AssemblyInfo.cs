b908d72bfe8779ddeb463bb62398913bbc9111af
Matthew Waldeck <112938730+MattWaldeck@users.noreply.github.com>
2025-06-13 11:07:31 +0200

    commit fixes

--- obj/Debug/net9.0/TodoApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6286f8b0308ec93066c7b9b5df2bf4c409847573")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb966b2f41c42a579d933cbfdde7bdff838ac081")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
